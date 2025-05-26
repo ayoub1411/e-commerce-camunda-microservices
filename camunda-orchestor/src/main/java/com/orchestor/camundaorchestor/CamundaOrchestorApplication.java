306f663d60be200fed363c57538c7d5583bf95e8
Ayoub HASSOINE <ayoub.hassoine@atos.net>
2025-05-26 13:27:54 +0200

    camunda dependencies

--- camunda-orchestor/src/main/java/com/orchestor/camundaorchestor/CamundaOrchestorApplication.java
+++ camunda-orchestor/src/main/java/com/orchestor/camundaorchestor/CamundaOrchestorApplication.java
@@ -1,10 +1,12 @@
 package com.orchestor.camundaorchestor;
 
+import org.camunda.bpm.engine.RuntimeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CamundaOrchestorApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(CamundaOrchestorApplication.class, args);
