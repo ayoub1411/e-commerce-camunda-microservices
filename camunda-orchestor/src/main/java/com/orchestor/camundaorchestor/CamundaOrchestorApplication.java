898422903f5f74d2fbf147a148666fbcd6cc220a
Ayoub HASSOINE <ayoub.hassoine@atos.net>
2025-05-26 13:15:18 +0200

    first commit

---
+++ camunda-orchestor/src/main/java/com/orchestor/camundaorchestor/CamundaOrchestorApplication.java
@@ -0,0 +1,13 @@
+package com.orchestor.camundaorchestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaOrchestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaOrchestorApplication.class, args);
+    }
+
+}
---
+++ camunda-orchestor/src/test/java/com/orchestor/camundaorchestor/CamundaOrchestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orchestor.camundaorchestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaOrchestorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
